1dc2685a26f5af7e6925913fd931bc16ee76b0d2
Fah22 <fadzisondoro@icloud.com>
2020-06-27 09:16:51 +0200

    Added

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const socket = io('http://localhost:3000')
 
+const messageForm = document.getElementById('send-container');
+const messageInput = document.getElementById('message-input')
+
 socket.on('chat-message', data => {
     console.log(data)
+})
+
+messageForm.addEventListener('submit', e => {
+    //to prevent the page from reloading on submit
+    e.preventDefault()
+    const message = messageInput.value 
+    socket.emit('send-chat-message', message)
+    //empty textbox after sending  message
+    messageInput.value = '';
 })
--- server.js
+++ server.js
@@ -3,5 +3,9 @@ const io = require('socket.io')(3000) //create a server by passing port number
 //everytime a user logs on this function is called
 io.on('connection', socket => {
     //'event-name', followed by 'message'
-    socket.emit('chat-message', 'Hello World')
+    //emit sends info from the client to the server
+    socket.on('send-chat-message', message => {
+        //sends message to whoever is connected to the server
+       socket.broadcast.emit('chat-message', message)
+    })
 })
